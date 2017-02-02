83b796e4ce3074664938a5df5c44850f9be7ad96
Yip <David.Yip@MarinaBaySands.com>
2017-02-02 15:55:09 +0800

    cf

--- messages/index.js
+++ messages/index.js
@@ -88,7 +88,7 @@ bot.dialog('/', new builder.IntentDialog()
                 session.preferredLocale(locale, function (err) {
                     if (!err) {
                         // Locale files loaded
-                        session.endDialog('locale_updated');
+                        session.endDialog(locale);
                         session.beginDialog('/start');
                     } else {
                         // Problem loading the selected locale
