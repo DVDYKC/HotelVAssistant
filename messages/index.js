63598ad4ee30609af92786834bb10564aa3c7eb3
Yip <David.Yip@MarinaBaySands.com>
2017-02-02 13:57:02 +0800

    mbs fix

--- messages/index.js
+++ messages/index.js
@@ -81,7 +81,7 @@ bot.dialog('/', new builder.IntentDialog()
                 .text('We are happy to assist you with any enquiry! Please tap "Start" to begin')
                 .images([
                     new builder.CardImage(session)
-                        .url('http://www.marinabaysands.com/content/dam/singapore/marinabaysands/master/main/home/Navigation%20Images/MBSLogo-200x75px-newNav-v3.png')
+                        .url('http://logos-download.com/wp-content/uploads/2016/05/Marina_Bay_Sands_logo_logotype.png')
                         .alt('MBS')
                 ])
                 .buttons([
