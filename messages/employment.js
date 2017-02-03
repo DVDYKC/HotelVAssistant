d4a9fdcf18f5b78f2fd7c2a28052890621f3b25c
Yip <David.Yip@MarinaBaySands.com>
2017-02-03 16:09:03 +0800

    CL

--- messages/employment.js
+++ messages/employment.js
@@ -5,15 +5,17 @@ module.exports = {
     Dialog: [
         // Confirm check-in
         function (session) {
+            var options = session.localizer.gettext(session.preferredLocale(), "All_Yes_No");
             builder.Prompts.confirm(
                 session,
                 'Employement_FamilyMember',
+                options,
                 {
                     listStyle: builder.ListStyle.button
                 });
         },
         function (session, results) {
-            if (results.response) {
+            if (results.response == 'YES' || results.response == '是') {
                 return session.beginDialog('isemployed');
             } else {
                 return session.beginDialog('isnotemployed');
